38f1fe8acfc92c3cf09505b88ff193221b0af038
niawaromi <91767702+niawaromi@users.noreply.github.com>
2022-03-19 10:13:49 +0900

    Create loops.php

---
+++ loops.php
@@ -0,0 +1,15 @@
+<?php 
+// while 
+while (true) {
+    // Do something
+}
+// Loop with $counter
+$counter = 0;
+while ($counter < 10){
+    echo "Counter $counter", '<br>';
+    $counter++;
+}
+// do - white
+// for
+// foreach
+// Iterate ovwr associative array.
